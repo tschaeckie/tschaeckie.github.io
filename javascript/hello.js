8245fa106f8b46967d6300315d7f3c9a9c9cbebc
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-03-26 15:58:18 +0100

    link color js

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,1 +1,10 @@
 // aLert("Hello world");  Kommentare hier ausschalten mit Edit Toggle Line Comment
+
+// let message = " Hello World ";
+// alert(message);
+// message = "Hallo Welt";
+// alert(message);
+
+const LINK_COLOR = "#ff000";
+console.log("Link bitte in der Farbe", LINK_COLOR); 
+// f12 im Firefox drücken --> Konsole ist Developer Tools
