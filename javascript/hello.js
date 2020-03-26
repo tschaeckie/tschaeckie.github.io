a6a30fb29b2d8ddad9fa474918fe4c57b37a4056
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-03-26 17:39:37 +0100

    querySelector forEAch

--- javascript/hello.js
+++ javascript/hello.js
@@ -147,3 +147,15 @@ let firstParagraph = document.querySelector("#pFirst");
 console.log(firstParagraph);
 // firstParagraph.remove();
 firstParagraph.innerHTML = "Test";
+firstParagraph.style.color = "red";
+
+
+let indetedParas = document.querySelectorAll(".indent");
+console.log(indetedParas);
+
+indetedParas.forEach((para, index) => {
+        para.innerHTML = `Absatz ${index}`;
+        // para.style.color = "yellow";
+});
+
+
