e0160c151aa2de31eee0088f04dd9aaa69fd8659
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-03-26 17:42:09 +0100

    if jeder zweite Absatz Farbe ändern

--- javascript/hello.js
+++ javascript/hello.js
@@ -155,7 +155,14 @@ console.log(indetedParas);
 
 indetedParas.forEach((para, index) => {
         para.innerHTML = `Absatz ${index}`;
-        // para.style.color = "yellow";
+        if ( index % 2 == 0) {
+            para.style.color = "red";
+        } else {
+            para.style.color = "blue";
+        }
+        // beim if: jedes zweite wird rot angezeigt
 });
+
+
 
 
