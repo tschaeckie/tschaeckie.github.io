ec45687678b5202afbdbdf3f8b1c235691a71f22
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-03-26 16:02:07 +0100

    zeichenketten

--- javascript/hello.js
+++ javascript/hello.js
@@ -8,3 +8,18 @@
 const LINK_COLOR = "#ff000";
 console.log("Link bitte in der Farbe", LINK_COLOR); 
 // f12 im Firefox drücken --> Konsole ist Developer Tools
+
+let highscore = 520233
+console.log(highscore / 10);
+// numerische Variable
+
+let firstname = "John";
+let lastname ='Smith';
+console.log("Name: ", firstname, lastname);
+//Zeichenketten mit Anführungszeichen einfach oder doppelt - egal
+
+let fullname = 'Jeffrey "The Dude" Lebowski';
+console.log(fullname);
+// zur Anschauung, dass beide Anführungszeichenarten verwendet werden können
+
+
