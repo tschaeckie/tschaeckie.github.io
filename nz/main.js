0424952b975504e02fa5ea9c3460dba119568bbb
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-04-01 14:49:47 +0200

    set up leaflet

--- nz/main.js
+++ nz/main.js
@@ -1,2 +1,11 @@
 //wofür steht die 13 am Ende?
 var mymap = L.map('map').setView([-36.877474, 174.764108], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
