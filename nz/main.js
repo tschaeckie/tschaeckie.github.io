e0041bc35710591505cd064218e9716f8f46c451
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-04-01 16:11:54 +0200

    addTo(mymap)

--- nz/main.js
+++ nz/main.js
@@ -1,10 +1,10 @@
 //wofür steht die 13 am Ende?
 let map = document.querySelector("#map");
-let mymap = L.map('map').setView([-36.877474, 174.764108], 14);
+var mymap = L.map('map').setView([-36.877474, 174.764108], 13);
 
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-})
+}).addTo(mymap);
 
 let marker = L.marker([-36.877474, 174.764108]).addTo(mymap);
