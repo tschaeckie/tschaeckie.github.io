92a2e0fdbc4e0e1fc8294dce85da0529a39bf76b
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-04-01 16:50:28 +0200

    circle: Auckland City Center

--- nz/main.js
+++ nz/main.js
@@ -8,6 +8,18 @@ L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 }).addTo(mymap);
 
 let marker = L.marker([-36.878091, 174.764383]).addTo(mymap);
+//kann man die Farbe des Markers auch ändern?
 
 marker.bindPopup("<b>Mount Eden Summit</b><br>Gipfel des Mount Eden Vulcano Walks").openPopup();
 //gibt es auch ein Popup, das sich öffnet, wenn man mit der Maus darüber fährt??
+//Lage des Popups variabel? - rechts, links, oben, unten des markers?
+
+var circle = L.circle([-36.848455, 174.762183], {
+    color: 'green',
+    fillColor: 'green',
+    fillOpacity: 0.3,
+    radius: 600
+}).addTo(mymap);
+
+circle.bindPopup("<b>Auckland City Center</b>")
+//kann man immer nur ein Popup .openPopup öffnen??
