4aca0e0f21cf8256e60df399a676675bfd0d2253
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-04-02 17:20:54 +0200

    data.js

--- world/main.js
+++ world/main.js
@@ -2,7 +2,7 @@ let startLayer = L.tileLayer.provider("OpenTopoMap");
 
 
 let map = L.map("map", {
-    center: [0,0],
+    center: [0, 0],
     zoom: 2,
     layers: [
         startLayer
@@ -10,13 +10,13 @@ let map = L.map("map", {
 });
 
 L.control.layers({
-    "OpenTopoMap" : startLayer,
-    "OpenStreetMap.Mapnik" : L.tileLayer.provider("OpenStreetMap.Mapnik"),
-    "Stadia.AlidadeSmooth" : L.tileLayer.provider("Stadia.AlidadeSmooth"),
-    "Thunderforest.TransportDark" : L.tileLayer.provider("Thunderforest.TransportDark"),
-    "Stamen.Watercolor" : L.tileLayer.provider("Stamen.Watercolor"),
-    "Esri.WorldTerrain" : L.tileLayer.provider("Esri.WorldTerrain"),
-    "OpenStreetMap.HOT" : L.tileLayer.provider("OpenStreetMap.HOT"),
-
-}).addTo(map)
+    "OpenTopoMap": startLayer,
+    "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "Thunderforest.TransportDark": L.tileLayer.provider("Thunderforest.TransportDark"),
+    "Stamen.Watercolor": L.tileLayer.provider("Stamen.Watercolor"),
+    "Esri.WorldTerrain": L.tileLayer.provider("Esri.WorldTerrain"),
+    "OpenStreetMap.HOT": L.tileLayer.provider("OpenStreetMap.HOT"),
+}).addTo(map);
 //5-10 Layer hinzufügen https://leaflet-extras.github.io/leaflet-providers/preview/
+
+L.marker([0,0]).addTo(map);
