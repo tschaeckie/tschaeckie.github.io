3735602cd2b39c5c65466924b387d489213af47f
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-04-02 17:34:04 +0200

    marker

--- world/main.js
+++ world/main.js
@@ -20,3 +20,12 @@ L.control.layers({
 //5-10 Layer hinzufügen https://leaflet-extras.github.io/leaflet-providers/preview/
 
 L.marker([0,0]).addTo(map);
+
+//console.log(CONFIRMED);
+for (let i = 1; i < CONFIRMED.length; i++) {
+    let row = CONFIRMED[i];
+    //console.log(row[2],row[3]);
+    let val = row[row.lenth-1];
+    let mrk = L.marker([row[2],row[3]]).addTo(map); 
+    mrk bindPopup(`${row[0]} ${row[1]}: ${val}`);     
+}
