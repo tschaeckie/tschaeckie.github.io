d4a2750109386bfc2db2eeb367755bc5d17ce43e
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-04-02 18:00:59 +0200

    drawCircles

--- world/main.js
+++ world/main.js
@@ -9,6 +9,7 @@ let map = L.map("map", {
     ]
 });
 
+let cirleGroup = L.featureGroup().addTo(map);
 L.control.layers({
     "OpenTopoMap": startLayer,
     "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
@@ -16,31 +17,34 @@ L.control.layers({
     "Stamen.Watercolor": L.tileLayer.provider("Stamen.Watercolor"),
     "Esri.WorldTerrain": L.tileLayer.provider("Esri.WorldTerrain"),
     "OpenStreetMap.HOT": L.tileLayer.provider("OpenStreetMap.HOT"),
+}, {
+    "Thematische Darstellung": circleGroup
+
 }).addTo(map);
 //5-10 Layer hinzufügen https://leaflet-extras.github.io/leaflet-providers/preview/
 
-L.marker([0, 0]).addTo(map);
+let drawCircles = function (data) {
+    //console.log(CONFIRMED);
+    for (let i = 1; i < CONFIRMED.length; i++) {
+        let row = CONFIRMED[i];
+        //console.log(row[2],row[3]);
+        let reg = `${row[0]} ${row[1]}`;
+        let lat = row[2];
+        let lng = row[3];
+        let val = row[row.lenth - 1];
+        let mrk = L.marker([lat, lng], {
+            radius: (val / 2) * 0,
+            001
+        }).addTo(map);
+        //mrk bindPopup(`${reg}: ${val}`);
 
-//console.log(CONFIRMED);
-for (let i = 1; i < CONFIRMED.length; i++) {
-    let row = CONFIRMED[i];
-    //console.log(row[2],row[3]);
-    let reg = `${row[0]} ${row[1]}`;
-    let lat = row[2];
-    let lng = row[3];
-    let val = row[row.lenth - 1];
-    let mrk = L.marker([lat, lng], {
-        radius: (val/2)*0,001
-    }).addTo(map);
-    //mrk bindPopup(`${reg}: ${val}`);
-
-    //A = r²*PI
-    //r² = A/PI
-    //r= WURZEL(A/PI)
-    let r = Math.squrt(val/Math.PI); 
-    let circle = L.circleMarker([lat, lng], {
-        radius: r
-    }).addTo(map);
-    
-    circle.bindPopup(`${val}`)
-}
+        //A = r²*PI
+        //r² = A/PI
+        //r= WURZEL(A/PI)
+        let r = Math.squrt(val / Math.PI);
+        let circle = L.circleMarker([lat, lng], {
+            radius: r
+        }).addTo(circleGroup);
+        circle.bindPopup(`${reg}: ${val}`);
+    }
+};
