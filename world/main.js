7cee17d9422bd4d5faf5001489883f30d6086d43
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-04-02 16:43:44 +0200

    grundgerüst

---
+++ world/main.js
@@ -0,0 +1,17 @@
+let startLayer = L.tileLayer.provider("OpenTopoMap");
+
+let map = L.map("map", {
+    center: [0,0],
+    zoom: 2,
+    layers: [
+        startLayer
+    ]
+});
+
+L.control.layers({
+    "OpenTopoMap" : startLayer,
+    "OpenStreetMap.Mapnik" : L.tileLayer.provider("OpenStreetMap.Mapnik"),
+
+
+}).addTo(map)
+//5-10 Layer hinzufügen
