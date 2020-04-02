755f7bb886ac2087454e7967e59b7953e0934bd0
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-04-02 17:01:14 +0200

    layer

--- world/main.js
+++ world/main.js
@@ -1,5 +1,6 @@
 let startLayer = L.tileLayer.provider("OpenTopoMap");
 
+
 let map = L.map("map", {
     center: [0,0],
     zoom: 2,
@@ -11,7 +12,11 @@ let map = L.map("map", {
 L.control.layers({
     "OpenTopoMap" : startLayer,
     "OpenStreetMap.Mapnik" : L.tileLayer.provider("OpenStreetMap.Mapnik"),
-
+    "Stadia.AlidadeSmooth" : L.tileLayer.provider("Stadia.AlidadeSmooth"),
+    "Thunderforest.TransportDark" : L.tileLayer.provider("Thunderforest.TransportDark"),
+    "Stamen.Watercolor" : L.tileLayer.provider("Stamen.Watercolor"),
+    "Esri.WorldTerrain" : L.tileLayer.provider("Esri.WorldTerrain"),
+    "OpenStreetMap.HOT" : L.tileLayer.provider("OpenStreetMap.HOT"),
 
 }).addTo(map)
-//5-10 Layer hinzufügen
+//5-10 Layer hinzufügen https://leaflet-extras.github.io/leaflet-providers/preview/
