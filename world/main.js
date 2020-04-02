5a811ff8defd18661c437aa9491871d09decede0
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-04-02 17:47:12 +0200

    größenkreise nach fläche

--- world/main.js
+++ world/main.js
@@ -19,13 +19,28 @@ L.control.layers({
 }).addTo(map);
 //5-10 Layer hinzufügen https://leaflet-extras.github.io/leaflet-providers/preview/
 
-L.marker([0,0]).addTo(map);
+L.marker([0, 0]).addTo(map);
 
 //console.log(CONFIRMED);
 for (let i = 1; i < CONFIRMED.length; i++) {
     let row = CONFIRMED[i];
     //console.log(row[2],row[3]);
-    let val = row[row.lenth-1];
-    let mrk = L.marker([row[2],row[3]]).addTo(map); 
-    mrk bindPopup(`${row[0]} ${row[1]}: ${val}`);     
+    let reg = `${row[0]} ${row[1]}`;
+    let lat = row[2];
+    let lng = row[3];
+    let val = row[row.lenth - 1];
+    let mrk = L.marker([lat, lng], {
+        radius: (val/2)*0,001
+    }).addTo(map);
+    //mrk bindPopup(`${reg}: ${val}`);
+
+    //A = r²*PI
+    //r² = A/PI
+    //r= WURZEL(A/PI)
+    let r = Math.squrt(val/Math.PI); 
+    let circle = L.circleMarker([lat, lng], {
+        radius: r
+    }).addTo(map);
+    
+    circle.bindPopup(`${val}`)
 }
