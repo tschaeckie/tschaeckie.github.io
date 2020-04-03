6f71f1659a1b3688a214115eb8e53269ce4f064c
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-04-03 18:36:23 +0200

    control layers

--- world_nachbereitung/main.js
+++ world_nachbereitung/main.js
@@ -1,8 +1,26 @@
+
+//nachdem Grundkarte zugefügt wurde, startLayer festlegen: OpenTopoMap
+let startLayer = L.tileLayer.provider("Esri.WorldTopoMap");
+
 //Grundkarte hinzufügen
 let map = L.map("map", {
     center: [0,0],
     zoom: 2,
     layers: [
-        L.tileLayer.provider("OpenTopoMap")
+       //gelöscht bei layer Control: L.tileLayer.provider("OpenTopoMap")
+       startLayer
     ]
 });
+
+//Layer Control
+//andere Layer unter: https://leaflet-extras.github.io/leaflet-providers/preview/
+//Google: Leaflet Providers: https://github.com/leaflet-extras/leaflet-providers
+//Providers Previewed
+L.control.layers({
+    "OpenTopoMap" :  L.titleLayer.provider("OpenTopoMap"),
+    "OpenStreetMap.Mapnik" : L.tileLayer.provider("OpenStreetMap.Mapnik"),
+
+
+
+
+}).addTo(map)
