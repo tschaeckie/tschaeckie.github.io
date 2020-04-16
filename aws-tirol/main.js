aba8cb3a68d5100504561970653719a58bb33fda
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-04-16 17:35:14 +0200

    add aws layer to basemap, feature group ein und ausschalten

---
+++ aws-tirol/main.js
@@ -0,0 +1,36 @@
+let startLayer = L.tileLayer.provider("BasemapAT.grau");
+
+let map = L.map("map", {
+    center: [47.3, 11.5],
+    zoom: 8,
+    layers: [
+        startLayer
+    ]
+});
+
+let awsLayer = L.featureGroup().addTo(map);
+
+L.control.layers({
+    "BasemapAT.grau": startLayer,
+    "BasemapAT": L.tileLayer.provider("BasemapAT"),
+    "BasemapAT.highdpi": L.tileLayer.provider("BasemapAT.highdpi"),
+    "BasemapAT.terrain": L.tileLayer.provider("BasemapAT.terrain"),
+    "BasemapAT.surface": L.tileLayer.provider("BasemapAT.surface"),
+    "BasemapAT.orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+    "BasemapAT.overlay": L.tileLayer.provider("BasemapAT.overlay"),
+    "BasemapAT.orthofoto+overlay": L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ])
+}, {
+    "Wetterstationen Tirol": awsLayer
+}).addTo(map);
+
+let awsUrl = "https://aws.openweb.cc/stations";
+
+let aws = L.geoJson.ajax(awsUrl, {
+    pointToLayer: function(point, latlng) {
+        console.log("point: ", point);
+        return L.marker(latlng);
+    }
+}).addTo(awsLayer);
