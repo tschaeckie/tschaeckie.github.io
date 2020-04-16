700fecd444cd7ec4e306c8917208eab993d81164
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-04-16 17:10:43 +0200

    Flächen inkl. Popup und <p>

--- wien/main.js
+++ wien/main.js
@@ -37,7 +37,7 @@ let walk = L.geoJson.ajax(walkUrl, {
         let marker = L.marker(latlng, {
             icon: icon
         });
-        console.log("Point", point);
+        //console.log("Point", point);
         marker.bindPopup(`<h3>${point.properties.NAME}</h3>
         <p><a target="links" href="${point.properties.WEITERE_INF}">Link</a></p>
         `);
@@ -52,10 +52,29 @@ walk.on("data:loaded", function() {
 });
 
 
+//Linien
+
 let wandern = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:WANDERWEGEOGD&srsName=EPSG:4326&outputFormat=json"
 
 L.geoJson.ajax(wandern, {
     style: function() {
         return { color: "green", weight: 5};
+    }
+}).addTo(map);
+
+
+//Flächen
+
+let heritage = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:WELTKULTERBEOGD&srsName=EPSG:4326&outputFormat=json"
+
+L.geoJson.ajax(heritage, {
+    style: function() {
+        return { color: "salmon", fillOpacity: 0.3 };
+    },
+    onEachFeature: function(feature, layer) {
+        console.log("Feature: ", feature);
+        layer.bindPopup(`<h3>${feature.properties.NAME}</h3>
+        <p>${feature.properties.INFO}</p>
+        `);
     }
 }).addTo(map);
