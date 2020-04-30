1b0ab2ac1c91749d0ed1f318150b426b1509538e
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-04-30 17:01:24 +0200

    clean up code

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -34,7 +34,7 @@ L.control.layers({
 
 //forof Schleife (gleich auswählen)
 for (const blick of ADLERBLICKE) {
-    console.log(blick);
+    //console.log(blick);
     let mrk = L.marker([blick.lat,blick.lng], {
         icon: L.icon({
             iconSize: [32, 37],
@@ -43,7 +43,7 @@ for (const blick of ADLERBLICKE) {
             iconUrl: "icons/panoramicview.png"
         })
     }).addTo(overlay.adlerblicke);
-    L.marker([blick.lat,blick.lng]).addTo(map);
+    //L.marker([blick.lat,blick.lng]).addTo(map);
     mrk.bindPopup(`Standort${blick.standort} (${blick.seehoehe}m)`);
 }
 overlay.adlerblicke.addTo(map);
