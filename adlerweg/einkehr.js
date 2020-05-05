b0414c9e28da7340ce1eef354990e4f18eb15ccb
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-05-05 15:30:09 +0200

    etappen.js als script einfügen; erste Koordinaten-Versuche Etappe 9

--- adlerweg/einkehr.js
+++ adlerweg/einkehr.js
@@ -1,7 +1,11 @@
 const EINKEHR = [
-    [Etappennummer(9), Einkehrmöglichkeit, Lat, Lng],
+    [Etappennummer(9), Lamsenjochhütte, 47.38005, 11.60356],
+    [Etappennummer(9), Binsalm, 47.39449, 11.58392],
+    [Etappennummer(9), Engalm, 47.39909, 11.55850],
+    [Etappennummer(9), Falkenhütte, 47.39972, 11.49895],
     [Etappennummer(10), Einkehrmöglichkeit, Lat, Lng],
     [Etappennummer(11), Einkehrmöglichkeit, Lat, Lng],
     [Etappennummer(12), Einkehrmöglichkeit, Lat, Lng],
+    [Etappennummer(13), Einkehrmöglichkeit, Lat, Lng],
     [Etappennummer(14), Einkehrmöglichkeit, Lat, Lng],
 ];
