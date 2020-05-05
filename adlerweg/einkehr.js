919a42e5425a340995469b1170f24d59f682e822
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-05-05 14:55:48 +0200

    Sonderzeichen Etappen

---
+++ adlerweg/einkehr.js
@@ -0,0 +1,7 @@
+const EINKEHR = [
+    [Etappennummer(9), Einkehrmöglichkeit, Lat, Lng],
+    [Etappennummer(10), Einkehrmöglichkeit, Lat, Lng],
+    [Etappennummer(11), Einkehrmöglichkeit, Lat, Lng],
+    [Etappennummer(12), Einkehrmöglichkeit, Lat, Lng],
+    [Etappennummer(14), Einkehrmöglichkeit, Lat, Lng],
+];
