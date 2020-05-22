46a1232a1da11b072865a5b49f70aba402192b15
tschaeckie <jacqueline.kowalski@student.uibk.ac.at>
2020-05-22 12:50:27 +0200

    leaflet plugin und Karte einfügen

---
+++ Tauernhoch/main.js
@@ -0,0 +1,18 @@
+let startLayer = L.tileLayer.provider("BasemapAT.grau");
+
+let map = L.map("map", {
+    center: [47.3, 11.5],
+    zoom: 8,
+    layers: [
+        startLayer
+    ]
+});
+
+L.control.layers({
+    "BasemapAT.grau": startLayer,
+    "BasemapAT": L.tileLayer.provider("BasemapAT"),
+    "BasemapAT.orthofoto+overlay": L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ])
+});
